cb6357141cac64daa856ec667dd88d0000b6506d
Arya Kamandanu <ezaalfandy.ea>
2016-11-01 04:58:57 +0700

    Intent Camera

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -1,12 +1,17 @@
 package id.sch.smktelkom_mlg.learn.intent3;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +42,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        findViewById(R.id.imageViewcamera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                capturephoto();
+
+            }
+        });
+
+
+
+
+
     }
 
     public void dialPhoneNumber(String phonenumber)//Belum DI tes
@@ -48,7 +66,6 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
     }
 
-
     public void composesmsmessage(String message)//Belum di tes
     {
         Intent intent = new Intent(Intent.ACTION_SEND);
@@ -59,7 +76,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     public void openWebPage(String url)//belum di tes
     {
         Uri webpage = Uri.parse(url);
@@ -67,6 +83,26 @@ public class MainActivity extends AppCompatActivity {
         if (intent.resolveActivity(getPackageManager()) != null)
             startActivity(intent);
     }
+
+    public void capturephoto()//belum di tes
+    {
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, requestCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bitmap bitmap = data.getParcelableExtra("data");
+            ImageView iv = (ImageView) findViewById(R.id.imageViewcamera);
+            iv.setImageBitmap(bitmap);
+        }
+    }
+
+
 
 
 }
